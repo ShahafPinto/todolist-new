b131c2db8be56b772249b03f3fd148b2c8c1c272
ShahafPinto <shahafchen7@gmail.com>
2022-12-08 15:45:05 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ import Footer from './components/Footer';
 import {useEffect, useState} from 'react';
 
 function App() {
-  // let todos = [
-  //   // {title: 'Learn React', completed: false, id:1},
-  //   // {title: 'Listen to Nir React', completed: false, id:2},
-  //   // {title: 'Learn JS', completed: false, id:3}
-  // ];
 
 const [todos, setTodos] = useState([]);
 const [nonCompletedItems, setNonCompletedItems] = useState(0)
 let leftItemsCounter = nonCompletedItems;
 const [editClass, setEditClass] = useState(false);
-
-// const [completedClass, setCompletedClass] = useState(false);
 
 useEffect(()=>{
   const nonComplete = todos.filter(todo=> todo.completed !== true)
@@ -33,48 +26,43 @@ useEffect(()=>{
   const removeTodo = (todoToRemove)=>{ 
     const newTodos = todos.filter( currentTodo => currentTodo.id != todoToRemove.id);
     setTodos(newTodos);
-    console.log(todos);
+    // console.log(todos);
   }
 
 
   const clearAllCompleted = ()=>{ 
+    console.log('start to del in all completed:',todos);
     const newTodos = todos.filter(currentTodo => !currentTodo.completed);
-    console.log(newTodos);
+    console.log('after del all completed:',newTodos);
     setTodos(newTodos);
   }
 
   const setComplete = (todoToComplete, checkedValue)=>{
-    console.log(`todoToComplete: ${todoToComplete} checkedValue:${checkedValue}`);
+    // console.log(`todoToComplete: ${todoToComplete} checkedValue:${checkedValue}`);
     todos.forEach((todo)=>{
         if (todo.id==todoToComplete.id){
             todo.completed=checkedValue;
-            // setTodos(newTodos);
         }
       }) 
     if (checkedValue){
       leftItemsCounter -=1 
-      // completedClass = true
     }
     else{
       leftItemsCounter+=1
-      // completedClass = false
     }  
-    
     setNonCompletedItems(leftItemsCounter);
-    // setCompletedClass(completedClass);
-    console.log(todos);
-    // setTodos(newTodos)
-    // console.log('setcomplete done');
+
   }
 
 const toggleAllItems = (checkedValue)=>{ 
-  console.log(checkedValue);
+  // console.log(checkedValue);
   const newTodos = todos.map(todo => ({...todo, completed: checkedValue}));
   console.log(newTodos);
-  // setTodos(newTodos);
+  setTodos(newTodos)
+  
 }
 const classToEditing= (item)=>{
-  console.log('here');
+  // console.log('here');
   if (item.editing){
     item.editing = false;
   }else{
@@ -83,11 +71,10 @@ const classToEditing= (item)=>{
   setEditClass(item.editing);
 }
 
-
   return (
     <div className="todoapp">
       <Header onAddItem={addTodo} />
-      <Main items={todos} onToggleAll={toggleAllItems} onDellItem={removeTodo} onSetComplete={setComplete}  onEditFunc={classToEditing}/>
+      <Main items={todos} onToggleAll={toggleAllItems} onDellItem={removeTodo} onSetComplete={setComplete}  onEditClass={classToEditing}/>
       <Footer onClearAll={clearAllCompleted} itemLeftCount={nonCompletedItems}/>
     </div>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -1,32 +1,26 @@
-import { useState } from "react";
 
 function List(props){
-    // const [editClass, setEditClass] = useState(false);
 
-    function hendlerItemToComplete(item, event){
-        // console.log(item);
-        props.onSetComplete(item ,event.target.checked);
-        // console.log('del func is on');
+    function hendlerEnter(event,item){
+        if (event.key == 'Enter'){
+            item.title = event.target.value;
+            props.onEditClass(item);
+        }
     }
-
-    function hendlerDoubleClickItem(item){
-        console.log('double clicked!');
-        props.onEditFunc(item);
-    }
-
     return(
         
         <ul className="todo-list">
             {props.items.map(item =>(
                 <li className= {'todo-list'+(item.completed ? ' completed' : '')+(item.editing? ' editing': '')}>
                     <div className="view">
-                        <input onChange={(event)=>hendlerItemToComplete(item,event)} className="toggle" type="checkbox" itemtitle={item.title}/>
-                        <label onDoubleClick={()=>hendlerDoubleClickItem(item)}>{item.title}</label>
+                        <input onChange={(event)=>props.onSetComplete(item ,event.target.checked)} 
+                                className="toggle" type="checkbox" checked={item.completed} itemtitle={item.title}/>
+                        <label onDoubleClick={()=>props.onEditClass(item)}>{item.title}</label>
                         <button onClick={()=>props.onDellItem(item)} 
                                 className="destroy" itemtitle={item.title}>
                         </button>
                     </div>
-                    <input className="edit" />
+                    <input className="edit" onKeyDown={(event)=>hendlerEnter(event,item)} defaultValue={item.title}/>
                 </li>
             ))}
         </ul>
